fb1bf8bb42a1d042fac848c365218467f5344a30
lournglgds <lournglgds@gmail.com>
2025-08-21 19:46:15 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,87 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("=== VARIABLES ===");
+
+// let firstName = "Jonas";
+// console.log(firstName);
+
+// let age = 30;
+// age = 31; 
+// console.log(age);
+
+// const birthYear = 1991;
+// console.log(birthYear);
+
+// const PI = 3.1415;
+// console.log(PI);
+
+// var job = "programmer";
+// job = "teacher";
+// console.log(job);
+
+// age = 26;
+
+// let lastName = "Doe";
+// console.log(lastName);
+
+
+// console.log("=== DATA TYPES ===");
+
+// let age = 23;
+// console.log(age);
+// console.log(typeof age); 
+
+// let firstName = "Jonas";
+// console.log(firstName);
+// console.log(typeof firstName); 
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun); 
+
+// let year;
+// console.log(year); 
+// console.log(typeof year);
+
+// let dynamicVariable = 23;
+// console.log(dynamicVariable, typeof dynamicVariable); 
+
+// dynamicVariable = "Now I'm a string!";
+// console.log(dynamicVariable, typeof dynamicVariable); 
+
+// dynamicVariable = true;
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+//EXERCISE 1: Personal Info Variables
+
+// const firstName = "Louren";
+// console.log(firstName);
+
+// let age = 20;
+// console.log(age);
+
+// const faveNumber = 7;
+// console.log(faveNumber);
+
+// const likesJavaScript = true;
+// console.log(likesJavaScript);
+
+// EXERCISE 2: Variable Reassignment
+
+// let score = 100;
+// console.log(score); 
+// score = 150;
+// console.log(score);
+
+// const score = 100;
+// console.log(score); // an error will appear because the variable score has already been declared.
+
+// EXERCISE 3: Data Type Detective
+console.log(typeof 42);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
